7ba2010aaeb54712a62ecf761f3b74ca3ccc0089
Mohamed Omara <martm7726@gmail.com>
2023-05-15 03:21:44 +0300

    Simple CRUD App with Collection

---
+++ spring-boot-rest-crud-employee/src/main/java/com/project/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.project.springboot.cruddemo.dao;
+
+import com.project.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
